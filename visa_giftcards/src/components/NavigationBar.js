adc534b076c6acee8a4662347480897120e61211
Nasuru <nasuruf@gmail.com>
2020-06-18 19:19:56 -0600

    Update NavigationBar.js

--- visa_giftcards/src/components/NavigationBar.js
+++ visa_giftcards/src/components/NavigationBar.js
@@ -23,9 +23,9 @@ export const NavigationBar = () => (
             <Navbar.Toggle area-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/my-gifts">My Gifts</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/#/">Home</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/#/my-gifts">My Gifts</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/#/login">Login</Nav.Link></Nav.Item>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
